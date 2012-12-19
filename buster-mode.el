b7dcabb66ee5bca3684a7bd4d08528331de9b782
Magnar Sveen <magnars@gmail.com>
2012-12-19 11:02:58 +0100

    Revert back to episode 06

--- buster-mode.el
+++ buster-mode.el
@@ -31,9 +31,6 @@
 
 ;;; Code:
 
-
-(require 'ansi-color)
-
 (defun buster-goto-current-test ()
   (search-backward-regexp "[\"'][^ ]* .+[\"']: function" nil t))
 
@@ -54,18 +51,10 @@
   (interactive)
   (buster-toggle-test-name-prefix "=>"))
 
-(defun buster--convert-ansi-escape-sequences (start end)
-  (goto-char start)
-  (while (search-forward "[1D" end t)
-    (delete-backward-char 5)))
-
 (defun buster-run-all-tests ()
   (interactive)
   (switch-to-buffer-other-window "*buster-test*")
-  (erase-buffer)
-  (call-process "buster-test" nil "*buster-test*" t)
-  (ansi-color-apply-on-region (point-min) (point-max))
-  (buster--convert-ansi-escape-sequences (point-min) (point-max)))
+  (call-process "buster-test" nil "*buster-test*" t))
 
 (defvar buster-mode-map (make-sparse-keymap)
   "buster-mode keymap")
